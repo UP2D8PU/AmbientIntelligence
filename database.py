1331a97f9cceff7e33681da1057e91db1ec7861d
charlotteheggem <charlotteheggem@hotmail.com>
2019-04-08 17:55:29 +0100

    plant database

---
+++ database.py
@@ -0,0 +1,24 @@
+
+plants = {
+    1: {
+        "name": "Rose",
+        "water quantity": 10,
+        "humidity threshold": 10,
+        "light intensity":10,
+    },
+    2:{
+        "name":"Cactus",
+        "water quantity": 10,
+        "humidity threshold": 10,
+        "light intensity":10,
+
+    },
+    3:{
+        "name":"Flower",
+        "water quantity": 10,
+        "humidity threshold": 10,
+        "light intensity":10,
+    }
+};
+
+#print (plants[1]["name"])
