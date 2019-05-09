ce2f4b9a77d65493148b71313f915be02dd43b56
ninamwa <ninamwa@stud.ntnu.no>
2019-05-09 21:01:20 +0100

    database updated

--- python/database.py
+++ python/database.py
@@ -24,7 +24,7 @@ plants = {
 
     },
     3:{
-        "name":"Flower",
+        "name":"Sunflower",
         "water quantity": 10,
         "humidity threshold min": 10,
         "humidity threshold max": 10,
@@ -36,37 +36,31 @@ garden = {
     0: {
         "type": 1,
         "angle": 0,
-        "water": 0,
         "humiditysensor value":0,
     },
     1: {
         "type": 0,
         "angle": 30,
-        "water": 0,
         "humiditysensor value":0,
     },
     2: {
         "type": 0,
         "angle": 60,
-        "water": 0,
         "humiditysensor value":0,
     },
     3: {
         "type": 0,
         "angle": 90,
-        "water": 0,
         "humiditysensor value":0,
     },
     4: {
         "type": 0,
         "angle": 120,
-        "water": 0,
         "humiditysensor value":0,
     },
     5: {
         "type": 0,
         "angle": 150,
-        "water": 0,
         "humiditysensor value":0,
     },
 
